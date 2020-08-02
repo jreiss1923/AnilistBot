0cc3e7ead5f695f4962e500d5480d706540821f1
jreiss1923 <joshua.reiss2@gmail.com>
2020-08-01 22:51:07 -0400

    I can do queries now yay

--- AnilistBot.py
+++ AnilistBot.py
@@ -1,5 +1,6 @@
 import discord
 import requests
+import time
 
 query = '''
 query ($name: String) { # Define which variables will be used in the query (id)
@@ -11,12 +12,41 @@ query ($name: String) { # Define which variables will be used in the query (id)
 '''
 
 # Define our query variables and values that will be used in the query request
-variables = {
-    'name': "patrui"
+variables_pat = {
+    'id': 121769
+}
+
+variables_hani = {
+    'id': 320308
+}
+
+variables_phil = {
+    'id': 163795
+}
+
+variables_alan = {
+    'id': 121839
+}
+
+variables_ben = {
+    'id': 122953
+}
+
+variables_zen = {
+    'id': 382311
+}
+
+variables_me = {
+    'id': 323865
 }
 
 url = 'https://graphql.anilist.co'
 
+arr_vars = [variables_alan, variables_ben, variables_hani, variables_pat, variables_phil, variables_zen, variables_me]
+
 # Make the HTTP Api request
-response = requests.post(url, json={'query': query, 'variables': variables})
-print(response.content)
+for variables in arr_vars:
+    response = requests.post(url, json={'query': query, 'variables': variables})
+    print(response.content)
+
+print(int(time.time()))
